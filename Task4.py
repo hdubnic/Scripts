dd5256fb8db6f77cb5e475eec3ea5efca5543cb8
hdubnic <hdubniczki@gmail.com>
2023-11-02 13:18:37 -0400

    code as tool in ArcGIS Pro

--- Task4.py
+++ Task4.py
@@ -6,19 +6,16 @@ arcpy.env.workspace = 'V:/ENV859_PS4/Data'
 # Allow us to overwrite output
 arcpy.env.overwriteOutput = True
 
-# Assign string variable to dataset of interest (in this case, I chose raster)
-inFC = 'counties.img' 
-
 # Get describe object
+inFC = arcpy.GetParameterAsText(0)
 desc = arcpy.da.Describe(inFC)
 
 # Indicate the dataset's catalogPath property 
 print("CatalogPath: " + desc["catalogPath"])
 
 # Report XMin, XMax, YMin, and YMax properties of the dataset's extent object
- extent = desc["extent"] # Assigning a string variable to the extent key in desc library
- print(("Extent: XMin: {0}, XMax: {1}, YMin: {2}, YMax: {3}".format(extent.XMin, extent.XMax, extent.YMin, extent.YMax)))  # Accessing values associated with extent key
-    #arcpy.AddMessage()
+extent = desc["extent"] # Assigning a string variable to the extent key in desc library
+arcpy.AddMessage((("Extent: XMin: {0}, XMax: {1}, YMin: {2}, YMax: {3}".format(extent.XMin, extent.XMax, extent.YMin, extent.YMax))))  # Accessing values associated with extent key
 
 # Check datasetType of our dataset
 dataset_type = desc["dataType"] # "dataType" in brackets because it is a key in a dictionary
@@ -29,15 +26,15 @@ if dataset_type == "RasterDataset":
     rows = arcpy.GetRasterProperties_management(inFC, "ROWCOUNT") # Searched for this function via Google
     columns = arcpy.GetRasterProperties_management(inFC, "COLUMNCOUNT")
 
-    print("Format: {}".format(f)) #arcpy.AddWarning
-    print("# of rows: {}".format(rows)) # arcpy.AddWarning
-    print("# of columns: {}".format(columns)) # arcpy.AddWarning
+    arcpy.AddWarning("Format: {}".format(f))
+    arcpy.AddWarning("# of rows: {}".format(rows))
+    arcpy.AddWarning("# of columns: {}".format(columns))
 
 if dataset_type == "FeatureClass":
     s = desc["shapeType"]
-    print("Shape Type: {}".format(s)) # arcpy.AddWarning
+    arcpy.AddWarning("Shape Type: {}".format(s)) 
 
 if dataset_type not in ["RasterDataset","FeatureClass"]:
-     print("Dataset Type: {}".format(dataset_type)) # arcpy.AddError
+    arcpy.AddError("Dataset Type: {}".format(dataset_type)) 
 
 # Note: each time I encoutnered an error, I pasted it into ChatGPT to troubleshoot the issue. I did not copy and paste solutions into my code. Rather, I cross-referenced ChatGPT, the class github page, and ESRI webpages to come to a solution. 
