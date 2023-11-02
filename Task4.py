a7eec95fd2352f379e2bb1921917e360f916eeda
hdubnic <hdubniczki@gmail.com>
2023-11-02 12:25:20 -0400

    final commit of Task4 (issues in ArcPro)

--- Task4.py
+++ Task4.py
@@ -6,11 +6,38 @@ arcpy.env.workspace = 'V:/ENV859_PS4/Data'
 # Allow us to overwrite output
 arcpy.env.overwriteOutput = True
 
-inFC = 'counties.img'       #arcpy.GetParameterAsText(0)
-outFC =       #arcpy.GetParameterAsText(1)
+# Assign string variable to dataset of interest (in this case, I chose raster)
+inFC = 'counties.img' 
 
+# Get describe object
 desc = arcpy.da.Describe(inFC)
 
-# Indicate the catalog path property 
+# Indicate the dataset's catalogPath property 
+print("CatalogPath: " + desc["catalogPath"])
 
-print("Feature Type:  " + desc.featureType)
+# Report XMin, XMax, YMin, and YMax properties of the dataset's extent object
+ extent = desc["extent"] # Assigning a string variable to the extent key in desc library
+ print(("Extent: XMin: {0}, XMax: {1}, YMin: {2}, YMax: {3}".format(extent.XMin, extent.XMax, extent.YMin, extent.YMax)))  # Accessing values associated with extent key
+    #arcpy.AddMessage()
+
+# Check datasetType of our dataset
+dataset_type = desc["dataType"] # "dataType" in brackets because it is a key in a dictionary
+
+if dataset_type == "RasterDataset":
+    # Assigning string variables to dictionary key and attributes of interest
+    f = desc["format"]
+    rows = arcpy.GetRasterProperties_management(inFC, "ROWCOUNT") # Searched for this function via Google
+    columns = arcpy.GetRasterProperties_management(inFC, "COLUMNCOUNT")
+
+    print("Format: {}".format(f)) #arcpy.AddWarning
+    print("# of rows: {}".format(rows)) # arcpy.AddWarning
+    print("# of columns: {}".format(columns)) # arcpy.AddWarning
+
+if dataset_type == "FeatureClass":
+    s = desc["shapeType"]
+    print("Shape Type: {}".format(s)) # arcpy.AddWarning
+
+if dataset_type not in ["RasterDataset","FeatureClass"]:
+     print("Dataset Type: {}".format(dataset_type)) # arcpy.AddError
+
+# Note: each time I encoutnered an error, I pasted it into ChatGPT to troubleshoot the issue. I did not copy and paste solutions into my code. Rather, I cross-referenced ChatGPT, the class github page, and ESRI webpages to come to a solution. 
