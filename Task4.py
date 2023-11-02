abcf587ca66da5eb6c96b74d4d44c2826a5cf544
hdubnic <hdubniczki@gmail.com>
2023-11-02 14:14:10 -0400

    final Task4 commit

--- Task4.py
+++ Task4.py
@@ -15,7 +15,7 @@ print("CatalogPath: " + desc["catalogPath"])
 
 # Report XMin, XMax, YMin, and YMax properties of the dataset's extent object
 extent = desc["extent"] # Assigning a string variable to the extent key in desc library
-arcpy.AddMessage((("Extent: XMin: {0}, XMax: {1}, YMin: {2}, YMax: {3}".format(extent.XMin, extent.XMax, extent.YMin, extent.YMax))))  # Accessing values associated with extent key
+arcpy.AddMessage(((" XMin: {0}\n XMax: {1}\n YMin: {2}\n YMax: {3}".format(extent.XMin, extent.XMax, extent.YMin, extent.YMax))))  # Accessing values associated with extent key
 
 # Check datasetType of our dataset
 dataset_type = desc["dataType"] # "dataType" in brackets because it is a key in a dictionary
@@ -30,11 +30,11 @@ if dataset_type == "RasterDataset":
     arcpy.AddWarning("# of rows: {}".format(rows))
     arcpy.AddWarning("# of columns: {}".format(columns))
 
-if dataset_type == "FeatureClass":
+if dataset_type == "ShapeFile":
     s = desc["shapeType"]
     arcpy.AddWarning("Shape Type: {}".format(s)) 
 
-if dataset_type not in ["RasterDataset","FeatureClass"]:
+if dataset_type not in ["RasterDataset","ShapeFile"]:
     arcpy.AddError("Dataset Type: {}".format(dataset_type)) 
 
 # Note: each time I encoutnered an error, I pasted it into ChatGPT to troubleshoot the issue. I did not copy and paste solutions into my code. Rather, I cross-referenced ChatGPT, the class github page, and ESRI webpages to come to a solution. 
